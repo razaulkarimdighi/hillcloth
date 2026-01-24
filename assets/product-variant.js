9edfed7894caebdf99d6d22ace02e69a6e083357
unknown <razaulk127@gmail.com>
2026-01-24 16:50:11 +0600

    updated

--- assets/product-variant.js
+++ assets/product-variant.js
@@ -1,19 +1,38 @@
- $(document).ready(function(){
-    function updateSeclection(){
-        var selectedValues = "";
-        $('.product_options input[type="radio"]:checked').each(function(){
-            selectedValues += (selectedValues ? " / " : "") + $(this).val();
-        });
-       //select the main variant
-       $(".variants option").each(function(){
-        if( $(this).text().toLowerCase().trim() == selectedValues.toLowerCase().trim() ){
-            $(this).prop("selected", true);
-            return false
-        }
-       })
+$(document).ready(function () {
+  function selectedVariant(param, value){
+    var url = new URL(window.location.href);
 
-    }
-    $('.product_options input[type="radio"]').change(updateSeclection);
+    url.searchParams.set(param, value)
+    window.history.replaceState({},'',url)
+  }
+  function updateSeclection() {
+    var selectedValues = "";
+    $('.product_options input[type="radio"]:checked').each(function () {
+      selectedValues += (selectedValues ? " / " : "") + $(this).val();
+    });
+
+    $(".product_options select option:selected").each(function () {
+      selectedValues += (selectedValues ? " / " : "") + $(this).val();
+    });
+
+    //select the main variant
+    $(".variants option").each(function () {
+      if (
+        $(this).text().toLowerCase().trim() ==
+        selectedValues.toLowerCase().trim()
+      ) {
+        $(this).prop("selected", true);
+        return false;
+      }
+    });
+  }
+  $('.product_options input[type="radio"]').change(function(){
     updateSeclection();
-    
- })
+
+    var currentVariant =  $(".variants").val();
+    console.log(currentVariant)
+    selectedVariant('variant', currentVariant);
+  });
+
+  updateSeclection();
+});
